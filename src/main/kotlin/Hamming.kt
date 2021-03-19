5b22cf0eb332a38df6b3b20109ae6d326b39d3ec
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-19 23:27:08 +0400

    Add completed solution

--- src/main/kotlin/Hamming.kt
+++ src/main/kotlin/Hamming.kt
@@ -1,6 +1,20 @@
+import java.lang.IllegalArgumentException
+
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        // Hamming distance is only valid for DNA strands of the same length
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+
+        var hammingDistance: Int = 0
+        for ((i, dnaStrand) in leftStrand.withIndex()) {
+            if (dnaStrand != rightStrand[i]) {
+                hammingDistance++
+            }
+        }
+
+        return hammingDistance
     }
 }
